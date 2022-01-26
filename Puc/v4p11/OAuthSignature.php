216503ac62b8ebec31546eb64ba4caf258b9f8c3
LaMonte M Forthun <lamontef@yahoo.com>
2022-01-26 02:32:34 -0500

    Update OAuthSignature.php

--- Puc/v4p11/OAuthSignature.php
+++ Puc/v4p11/OAuthSignature.php
@@ -25,7 +25,7 @@ if ( !class_exists('Puc_v4p11_OAuthSignature', false) ):
 			$parameters = array();
 
 			//Parse query parameters.
-			$query = parse_url($url, PHP_URL_QUERY);
+			$query = wp_parse_url($url, PHP_URL_QUERY);
 			if ( !empty($query) ) {
 				parse_str($query, $parsedParams);
 				if ( is_array($parameters) ) {
