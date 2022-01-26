00bc3efbeacf37f45db8f9f38d841e1921506325
LaMonte M Forthun <lamontef@yahoo.com>
2022-01-26 02:33:54 -0500

    Update Factory.php

--- Puc/v4p11/Factory.php
+++ Puc/v4p11/Factory.php
@@ -256,8 +256,8 @@ if ( !class_exists('Puc_v4p11_Factory', false) ):
 			$service = null;
 
 			//Which hosting service does the URL point to?
-			$host = parse_url($metadataUrl, PHP_URL_HOST);
-			$path = parse_url($metadataUrl, PHP_URL_PATH);
+			$host = wp_parse_url($metadataUrl, PHP_URL_HOST);
+			$path = wp_parse_url($metadataUrl, PHP_URL_PATH);
 
 			//Check if the path looks like "/user-name/repository".
 			//For GitLab.com it can also be "/user/group1/group2/.../repository".
